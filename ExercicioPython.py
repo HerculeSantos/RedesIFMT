da768232ee6836806a59a4aaa0824097895a7421
HerculeSantos <herculesss@gmail.com>
2020-02-14 10:18:06 -0400

    Introducao Laco Python

---
+++ ExercicioPython.py
@@ -0,0 +1,9 @@
+nome=""
+>>> usuarios=[]
+>>> while nome != "sair"
+  File "<stdin>", line 1
+    while nome != "sair"
+>>> while nome != "sair":
+...     nome = input("Nome do novo usuario: ")
+...     usuarios.append(nome)
+...
